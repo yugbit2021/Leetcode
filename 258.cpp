fcbe729d4405279af7f3a56f284954cbd853977d
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-10 18:20:29 +0530

    258

---
+++ 258.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10){
+            return num;
+        }
+         int sum=0;
+        while(num!=0){
+            int rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+        }
+        if(sum>=10){
+            return addDigits(sum);
+        }
+        return sum;
+    }
+};

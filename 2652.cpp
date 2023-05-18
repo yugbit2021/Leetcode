e92c95ca024d5db17815e183298fe66bae39de6d
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-18 11:04:02 +0530

    initaial

--- 2652.cpp
+++ 2652.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int sumOfMultiples(int n) {
-        vector<int> v;
-        for(int i=0;i<n;i++){
-            v.push_back(i+1);
-        }
+       
         int sum=0;
-        for(int i=2;i<n;i++){
-            if(v[i]%3==0){
-                sum=sum+v[i];
+        for(int i=3;i<=n;i++){
+            if(i%3==0){
+                sum=sum+i;
             }
-             else if(v[i]%5==0){
-                sum=sum+v[i];
+             else if(i%5==0){
+                sum=sum+i;
             }
-             else if(v[i]%7==0){
-                sum=sum+v[i];
+            else if(i%7==0){
+                sum=sum+i;
             }
         }
         return sum;

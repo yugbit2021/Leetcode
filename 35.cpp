2eb5988ed38e6e41838fd3ca4ce04d08e1fd0fa4
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-19 11:31:04 +0530

    35

---
+++ 35.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+
+        int ans=nums.size();
+
+        while(s<=e){
+           int mid=s+(e-s)/2;
+
+           if(nums[mid]==target){
+               return mid;
+           }
+           else if(nums[mid]<target){
+               s=mid+1;
+           }
+           else{
+              
+               ans= mid;
+               e=mid-1;
+               
+           }
+        }
+        return ans;
+    }
+};

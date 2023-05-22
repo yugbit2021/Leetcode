d4075bd343d3241b1b520d2e5fba09cd6a1bf67e
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-22 07:32:51 +0530

    ..

---
+++ 47.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<vector<int>> v={{1,1,2},{1,1,2}};
+    set<vector<int>> set;
+   for(auto x:v){
+    set.insert(x);
+   }
+}
+    
